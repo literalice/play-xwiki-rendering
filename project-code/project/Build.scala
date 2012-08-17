d1f0fb7d463e6cd84a0f80620fad5291bfa576d6
literalice <literalice@monochromeroad.com>
2012-08-18 08:37:29 +0900

    dependencies to XWiki libraries

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -4,15 +4,31 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "play-xwiki-rendering"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "play-xwiki-rendering"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val xwikiVersion = "4.1.3"
+  val xwikiGroup = "org.xwiki"
+  val xwikiCommonsGroup = xwikiGroup + ".commons"
+  val xwikiRenderingGroup = xwikiGroup + ".rendering"
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val xwikiMacros = Seq("xwiki21", "xhtml")
+  val xwikiMacrosDependencies = xwikiMacros.map(
+  { n => xwikiRenderingGroup % ("xwiki-rendering-syntax-" + n) % xwikiVersion })
+
+  val xwikiTransforms = Seq("macro")
+  val xwikiTransformsDependencies = xwikiTransforms.map(
+  { n => xwikiRenderingGroup % ("xwiki-rendering-transformation-" + n) % xwikiVersion })
+
+  val xwikiDependencies = Seq(
+    xwikiCommonsGroup % "xwiki-commons-component-default" % xwikiVersion
+  ) ++ xwikiMacrosDependencies ++ xwikiTransformsDependencies
+
+  val appDependencies = xwikiDependencies
+
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    organization := "com.monochromeroad.play",
+    resolvers += "XWiki Repository" at "http://nexus.xwiki.org/nexus/content/groups/public"
+  )
 
 }
