ee93155447268441f452ffab1102e4fe06b3cc92
literalice <literalice@monochromeroad.com>
2012-08-27 08:24:51 +0900

    adds logging

--- src/main/scala/com/monochromeroad/play/xwiki/rendering/DefaultXWikiRenderingPlugin.scala
+++ src/main/scala/com/monochromeroad/play/xwiki/rendering/DefaultXWikiRenderingPlugin.scala
@@ -12,7 +12,11 @@ class DefaultXWikiRenderingPlugin(app: Application) extends Plugin {
 
   private final val KEY_MACRO_ENABLED: String = KEY_MACRO + ".enabled"
 
+  val name = "XWiki Default Renderer"
+
   override def onStart() {
+    Logger.info("Configuring XWiki default renderers ...")
+
     val macroManager = new XWikiMacroManager(DefaultXWikiComponentManager)
 
     val macroListKeys = app.configuration.keys filter(p => p.startsWith(KEY_MACRO) && !p.startsWith(KEY_MACRO_ENABLED))
@@ -20,20 +24,38 @@ class DefaultXWikiRenderingPlugin(app: Application) extends Plugin {
 
     macroList.map({macroName =>
       loadMacroClass(macroName).map(macroManager.registerMacro(_))
+      Logger.info("Registered the macro: " + macroName)
     })
 
     val xwikiConfiguration = loadConfiguration(app.configuration)
     Seq(DefaultXWikiRenderer,
         DefaultXWikiStreamRenderer,
         DefaultXWikiStringStreamRenderer).map(_.configure(xwikiConfiguration))
+
+    Logger.info("XWiki default renderers configuration done.\n" + xwikiConfiguration)
   }
 
   private def loadMacroClass(macroClassName: String): Option[Class[DefaultXWikiMacro[_]]] = {
-    val mayMacroClass = Class.forName(macroClassName)
-    if (mayMacroClass.isInstanceOf[Class[DefaultXWikiMacro[_]]]) {
-      Some(mayMacroClass.asInstanceOf[Class[DefaultXWikiMacro[_]]])
-    } else {
-      None
+    findMacroClass(macroClassName) match {
+      case Some(mc) =>
+        if (mc.isInstanceOf[Class[DefaultXWikiMacro[_]]]) {
+          Some(mc.asInstanceOf[Class[DefaultXWikiMacro[_]]])
+        } else {
+          None
+        }
+      case None =>
+        Logger.warn("The XWiki macro (" + macroClassName + ") not found.")
+        None
+    }
+  }
+
+  private def findMacroClass(macroClassName: String): Option[Class[_]] = {
+    try {
+      Some(Class.forName(macroClassName))
+    } catch {
+      case _: ClassNotFoundException =>
+        Logger.warn("The XWiki macro (" + macroClassName + ") not found.")
+        None
     }
   }
 
--- src/main/scala/com/monochromeroad/play/xwiki/rendering/XWikiRendererConfiguration.scala
+++ src/main/scala/com/monochromeroad/play/xwiki/rendering/XWikiRendererConfiguration.scala
@@ -9,4 +9,11 @@ class XWikiRendererConfiguration(
           val defaultInputSyntax: Syntax = Syntax.XWIKI_2_1,
           val defaultOutputSyntax: Syntax = Syntax.XHTML_1_0,
           val macrosEnabled: Boolean = true) {
+
+  override def toString: String = {
+    "Default Syntax : " + defaultInputSyntax.toIdString + "\n" +
+    "Default Output : " + defaultOutputSyntax.toIdString + "\n" +
+    "Macros Active? : " + macrosEnabled
+  }
 }
+
