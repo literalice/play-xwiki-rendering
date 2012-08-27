998198c4cc99c030bd25e37b99572ae74fccd80b
literalice <literalice@monochromeroad.com>
2012-08-27 18:25:54 +0900

    release 1.0

--- samples/play-xwiki-rendering-sample/project/Build.scala
+++ samples/play-xwiki-rendering-sample/project/Build.scala
@@ -8,11 +8,12 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0"
  
   val appDependencies = Seq(
-    "com.monochromeroad" %% "play-xwiki-rendering" % "1.0-SNAPSHOT",
+    "com.monochromeroad" %% "play-xwiki-rendering" % "1.0",
     "org.xwiki.rendering" % "xwiki-rendering-macro-comment" % "4.1.3"
   )
  
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    resolvers += "Local Maven Repository" at "file:///" + Path.userHome.absolutePath + "/.m2/repository"
+    resolvers += "Monochrmeroad CloudBees Repository" at "http://repository-monochromeroad.forge.cloudbees.com/release"
+    // resolvers += "Local Maven Repository" at "file:///" + Path.userHome.absolutePath + "/.m2/repository"
   )
 }
